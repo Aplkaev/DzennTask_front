6d3bc727000bfee12054f84910750e4ddbabc163
Ruslan <almakaev-2011@mail.ru>
2025-08-25 09:28:08 +0500

    chore: remove debug console.log

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,6 @@ import { router } from './Router';
 import { useEffect } from 'react';
 
 export default function App() {
-  useEffect(() => {
-    const init = async () => {
-      console.log('user effect load app');
-    };
-    init();
-  }, []);
   return (
     <Provider>
       <RouterProvider router={router} />
